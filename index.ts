1f1dd9596aacb6f78ab3269d04e8b959b0e8d350
unknown <simramajeed2004@gmail.com>
2024-03-22 21:51:56 +0500

    updated the code and added an extra line at the end of the program

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const answer= await inquirer.prompt([{ message: "Enter first number",type: "number", name: "firstNumber" },
-{ message: "Enter Second number",type: "number", name: "secondNumber" },
+const answer= await inquirer.prompt([{ message: "Enter your first number",type: "number", name: "firstNumber" },
+{ message: "Enter your Second number",type: "number", name: "secondNumber" },
 
 {
     message: "Select one of the operators to perform operation",
@@ -25,3 +25,4 @@ else if (answer.operator==="Division") {
 else{
     console.log("Please input valid number");
 }
+console.log("THE END");
